15a9bc2a1ed7868995a97d1679eb3c3980f9eb27
iKrishneel <krishneel.chand.chaudhary@gmail.com>
2017-06-14 23:04:56 +0900

    updated.

--- scripts/data_argumentation_layer/argumentation_engine.py
+++ scripts/data_argumentation_layer/argumentation_engine.py
@@ -11,6 +11,11 @@ import random
 import numpy as np
 import cv2 as cv
 
+import imgaug.imgaug as ia
+import imgaug.imgaug
+import imgaug.imgaug.augmenters as iaa
+
+
 """
 Class for computing intersection over union(IOU)
 """
@@ -57,7 +62,6 @@ class ArgumentationEngine(object):
         
         self.__jc = JaccardCoeff()
         self.FORE_PROB_ = float(1.0)
-        self.BACK_PROB_ = float(1.0)
         self.FLT_EPSILON_ = sys.float_info.epsilon
 
     def bounding_box_parameterized_labels(self, img, rect, label, stride):
@@ -91,14 +95,12 @@ class ArgumentationEngine(object):
                     size_labels[k + 2, j, i] = 1.0 / rect[2]
                     size_labels[k + 3, j, i] = 1.0 / rect[3]
 
-                    diff = float(boxes[j, i][2] * boxes[j, i][3]) / float(rect[2] * rect[3])
+                    diff = float(boxes[j, i][2] * boxes[j ,i][3]) / float(rect[2] * rect[3])
                     obj_labels[k:k+channel_stride, j, i] = diff
 
                     coverage_label[k:k+channel_stride, j, i] = region_labels[j, i]
                     
                     foreground_labels[int(label), j, i] = self.FORE_PROB_
-                else:
-                    foreground_labels[int(0), j, i] = self.BACK_PROB_  #! learn the background as well
 
         return (foreground_labels, boxes_labels, size_labels, obj_labels, coverage_label)
 
@@ -107,7 +109,7 @@ class ArgumentationEngine(object):
         img_list = []
         label_resize = []
         if resize_flag:
-            img = cv.resize(image, resize_flag)
+            img = cv.resize(image, resize_flag, cv.INTER_CUBIC)
             img_list.append(img)
             # resize label
             ratio_x = float(image.shape[1]) / float(img.shape[1])
@@ -133,31 +135,32 @@ class ArgumentationEngine(object):
         scale_x = int(math.floor(scale_x))
         scale_y = int(math.floor(scale_y))
         
-        flip_flag = random.randint(-1, 1)
-        img_flip, rect_flip = self.flip_image(image.copy(), rect, flip_flag)
-        
-        return (img_flip, rect_flip)
-        
-        #!TODO
-        ##! flip and save
-        if scale_x < 3:
-            scale_x = 3
-        if scale_y < 3:
-            scale_y = 3
+        #! flip image
+        flip_flag = random.randint(-1, 2)
 
-        enlarge_factor1 = random.randint(2, scale_x)
-        enlarge_factor2 = random.randint(2, scale_y)
+        if flip_flag < 2:
+            img_flip, rect_flip = self.flip_image(image.copy(), rect, flip_flag)
+        else:
+            img_flip = image.copy()
+            rect_flip = rect
+        
+        #! zoom in
+        enlarge_factor1 = random.uniform(1.0, float(scale_x))
+        enlarge_factor2 = random.uniform(1.0, float(scale_y))
         widths = (int(rect_flip[2] * enlarge_factor1), rect_flip[2] * enlarge_factor2)
         heights = (int(rect_flip[3] * enlarge_factor1), rect_flip[3] * enlarge_factor2)
-        crop_image, crop_rect = self.crop_image_dimension(img_flip, rect_flip, widths, heights)
+        crop_image, crop_rect = self.crop_image_dimension(img_flip, rect_flip, \
+                                                          widths, heights)
 
-        ## apply blur
-        kernel_x = random.randint(3, 7)
-        kernel_y = random.randint(3, 7)
-        kernel_x = kernel_x + 1 if kernel_x % 2 is 0 else kernel_x
-        kernel_y = kernel_y + 1 if kernel_y % 2 is 0 else kernel_y
+        #! color arugmentation
+        crop_image = self.color_space_argumentation(crop_image)
 
-        blur_img = cv.GaussianBlur(crop_image, (kernel_x, kernel_y), 0)
+        #! rotate the image
+        rot_image, rot_rect = self.rotate_image_with_rect(crop_image, crop_rect)
+
+        #! normalize image
+        rot_mat = self.demean_rgb_image(rot_image)
+        return (rot_image, rot_rect)
 
 
     def crop_image_dimension(self, image, rect, widths, heights):
@@ -238,3 +241,53 @@ class ArgumentationEngine(object):
         im_rgb[:, :, 2] -= float(122.6789143406786)
         im_rgb = (im_rgb - im_rgb.min())/(im_rgb.max() - im_rgb.min())
         return im_rgb
+
+    """
+    Function that aguments the image color space
+    """
+    def color_space_argumentation(self, image):
+        seq = iaa.Sequential([
+            iaa.OneOf([
+                iaa.GaussianBlur((0, 3.0)),
+                iaa.AverageBlur(k=(2, 7)),
+                iaa.MedianBlur(k=(3, 7)),
+            ]),
+            iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),
+            iaa.Add((-2, 21), per_channel=0.5),
+            iaa.Multiply((0.75, 1.25), per_channel=0.5),
+            iaa.ContrastNormalization((0.5, 1.50), per_channel=0.5),
+            iaa.Grayscale(alpha=(0.0, 0.50)),
+        ],
+                             random_order=False)
+        return seq.augment_image(image)
+
+    """
+    Function to rotate image and rect by random angle
+    """
+    def rotate_image_with_rect(self, image, rect):
+        center = (image.shape[1]/2, image.shape[0]/2)
+        angle = float(random.randint(0, 360))
+        rot_mat = cv.getRotationMatrix2D(center, angle, 1)
+        im_rot = cv.warpAffine(image, rot_mat, (image.shape[1], image.shape[0]))
+        
+        #! rotate rect
+        x1 = rect[0] 
+        y1 = rect[1]
+        x2 = x1 + rect[2]
+        y2 = y1 + rect[3]
+        pt1x = int(x1 * rot_mat[0, 0] + y1 * rot_mat[0, 1] + rot_mat[0, 2])
+        pt1y = int(x1 * rot_mat[1, 0] + y1 * rot_mat[1, 1] + rot_mat[1, 2])
+        pt2x = int(x2 * rot_mat[0, 0] + y1 * rot_mat[0, 1] + rot_mat[0, 2])
+        pt2y = int(x2 * rot_mat[1, 0] + y1 * rot_mat[1, 1] + rot_mat[1, 2])
+        pt3x = int(x1 * rot_mat[0, 0] + y2 * rot_mat[0, 1] + rot_mat[0, 2])
+        pt3y = int(x1 * rot_mat[1, 0] + y2 * rot_mat[1, 1] + rot_mat[1, 2])
+        pt4x = int(x2 * rot_mat[0, 0] + y2 * rot_mat[0, 1] + rot_mat[0, 2])
+        pt4y = int(x2 * rot_mat[1, 0] + y2 * rot_mat[1, 1] + rot_mat[1, 2])
+        minx = min(pt1x, min(pt2x, min(pt3x, pt4x)))
+        miny = min(pt1y, min(pt2y, min(pt3y, pt4y)))
+        maxx = max(pt1x, max(pt2x, max(pt3x, pt4x)))
+        maxy = max(pt1y, max(pt2y, max(pt3y, pt4y)))
+
+        rect_rot = np.array([minx, miny, maxx-minx, maxy-miny])
+
+        return im_rot, rect_rot
--- scripts/data_argumentation_layer/data_argumentation_layer.py
+++ scripts/data_argumentation_layer/data_argumentation_layer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
+import os
 import caffe
-import math
-import random as rng
 import numpy as np
 import cv2 as cv
 import argumentation_engine as ae
@@ -11,8 +10,8 @@ class DataArgumentationLayer(caffe.Layer):
 
     def setup(self, bottom, top):
         #! check that inputs are pair of image and labels (bbox and label:int)
-        if len(bottom) != 2:
-            raise Exception('Need two inputs for Argumentation')
+        if len(bottom) > 0:
+            raise Exception('This layer takes no bottom')
         
         if len(top) < 6:
             raise Exception('Current Implementation needs 6 top blobs')
@@ -23,19 +22,30 @@ class DataArgumentationLayer(caffe.Layer):
             self.image_size_y = int(plist[1])
             self.stride = int(plist[2])
             self.num_classes = int(plist[3])
+            
+            self.batch_size = int(plist[4])
+            self.train_fn = str(plist[5]) #! dataset textfile
+            #self.randomize = bool(plist[6])
+            self.randomize = True
+
+            if not os.path.isfile(self.train_fn):
+                raise ValueError('Provide the dataset textfile')
+
+            else:
+                self.img_path, self.rects, self.labels = self.read_and_decode_lines()
+                self.idx = 0 #! start index
 
             self.__ae = ae.ArgumentationEngine(self.image_size_x, self.image_size_y, \
                                                self.stride, self.num_classes)
 
         except ValueError:
             raise ValueError('Parameter string missing or data type is wrong!')
+
+        
             
     def reshape(self, bottom, top):
         #! check input dimensions
-        if bottom[1].count < 5:
-            raise Exception('Labels should be 5 dimensional vector')
-                
-        n_images = bottom[0].data.shape[0]        
+        n_images = self.batch_size
         out_size_x = int(self.image_size_x / self.stride)
         out_size_y = int(self.image_size_y / self.stride)
 
@@ -47,19 +57,21 @@ class DataArgumentationLayer(caffe.Layer):
         top[2].reshape(n_images, channels, out_size_y, out_size_x) #! bbox labels # 4
         top[3].reshape(n_images, channels, out_size_y, out_size_x) #! size labels # 4
         top[4].reshape(n_images, channels, out_size_y, out_size_x) #! obj labels  # 4
-        top[5].reshape(n_images, channels, out_size_y, out_size_x) #! cvg block   # 4        
+        top[5].reshape(n_images, channels, out_size_y, out_size_x) #! cvg block   # 4
+        
         
     def forward(self, bottom, top):
-        for index, (data, labels) in enumerate(zip(bottom[0].data, bottom[1].data)):
-            img = data.copy()
-            rect = labels[0, 0, 0:4].copy()
-            label = labels[0, 0, 4].copy()
 
+        for index in xrange(0, self.batch_size, 1):
+            img = cv.imread(self.img_path[self.idx])
+            rect = self.rects[self.idx]
+            label = self.labels[self.idx]
+            
             img, rect = self.__ae.random_argumentation(img, rect)
             img, rect = self.__ae.resize_image_and_labels(img, rect)
             foreground_labels, boxes_labels, size_labels, obj_labels, coverage_label = \
             self.__ae.bounding_box_parameterized_labels(img, rect, label, self.stride)
-            
+
             img = img.swapaxes(2, 0)
             img = img.swapaxes(2, 1)
             
@@ -70,11 +82,55 @@ class DataArgumentationLayer(caffe.Layer):
             top[4].data[index] = obj_labels.copy()
             top[5].data[index] = coverage_label.copy()
 
-            for f in foreground_labels:
-                print f
-            print '---------------\n'
+            self.idx += 1
+            if self.idx >= len(self.img_path):
+                self.idx = 0
+
+        # for index, (data, labels) in enumerate(zip(bottom[0].data, bottom[1].data)):
+        #     img = data.copy()
+        #     rect = labels[0, 0, 0:4].copy()
+        #     label = labels[0, 0, 4].copy()
+
+        #     img, rect = self.__ae.random_argumentation(img, rect)
+        #     img, rect = self.__ae.resize_image_and_labels(img, rect)
+        #     foreground_labels, boxes_labels, size_labels, obj_labels, coverage_label = \
+        #     self.__ae.bounding_box_parameterized_labels(img, rect, label, self.stride)
             
+        #     img = img.swapaxes(2, 0)
+        #     img = img.swapaxes(2, 1)
+            
+        #     top[0].data[index] = img
+        #     top[1].data[index] = foreground_labels.copy()
+        #     top[2].data[index] = boxes_labels.copy()
+        #     top[3].data[index] = size_labels.copy()
+        #     top[4].data[index] = obj_labels.copy()
+        #     top[5].data[index] = coverage_label.copy()
+
     def backward(self, top, propagate_down, bottom):
         pass
         
 
+    """
+    Function to read and extract path to images, bounding boxes and the labels
+    """
+    def read_and_decode_lines(self):
+        lines = [line.rstrip('\n')
+                 for line in open(self.train_fn)
+        ]
+        
+        if self.randomize:
+            np.random.shuffle(lines)
+
+        img_path = []
+        rects = []
+        labels = []
+        for line in lines:
+            line_val = line.split()
+            img_path.append(line_val[0])
+            rect = []
+            for i in xrange(1, len(line_val)-1, 1):
+                rect.append(int(line_val[i]))
+            rects.append(rect)
+            labels.append(int(line_val[-1]))
+        
+        return np.array(img_path), np.array(rects), np.array(labels)
